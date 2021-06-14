98549b833d0b0e6fc2cd1881007185b213de82d6
LuftmenshTrepverter <71112445+LuftmenshTrepverter@users.noreply.github.com>
2021-06-14 14:02:56 +0800

    Add files via upload

---
+++ AlgoStruct10_5_3_Kruskal.cpp
@@ -0,0 +1,96 @@
+﻿#include <cstdio>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+const int MAXV = 110;
+const int MAXE = 10010;
+
+//边的结构体
+struct Edge
+{
+    int u, v;
+    int cost;
+}E[MAXE];
+
+//边权比较函数的cmp
+bool cmp(Edge a, Edge b)
+{
+    return a.cost < b.cost;
+}
+
+int father[MAXV];
+
+//并查集的查找和路径压缩功能的实现
+int findFather(int x)
+{
+    int a = x;
+    //寻找根结点
+    while (x != father[x])
+    {
+        x = father[x];
+    }
+
+    //路径压缩
+    while (a != father[a])
+    {
+        int z = a;
+        a = father[a];
+        father[z] = x;
+    }
+    return x;
+}
+
+int kruskal(int n, int m)
+{
+    int ans = 0, num_edge = 0;
+    
+    //并查集初始化
+    for (int i = 0; i < n; i++)
+    {
+        father[i] = i;
+    }
+
+    //所有边按边权排序
+    sort(E, E + m, cmp);
+    
+    //枚举所有的边
+    for (int i = 0; i < m; i++)
+    {
+        int U_father = findFather(E[i].u);
+        int V_father = findFather(E[i].v);
+        if (U_father != V_father) //如果不是一个集合
+        {
+            father[U_father] = V_father; //合并集合
+            ans += E[i].cost; //最小边权累加
+            num_edge++; //边数累加，边数是算法结束的条件
+            if (num_edge == n-1) //边数=顶点数-1，就退出算法
+            {
+                break;
+            }
+        }
+    }
+
+    if (num_edge != n-1) //图不连通
+    {
+        return -1;
+    }
+    else
+    {
+        return ans;
+    }
+}
+
+int main()
+{
+    int n, m;
+    scanf_s("%d%d", &n, &m);
+    for (int i = 0; i < m; i++)
+    {
+        scanf_s("%d%d%d", &E[i].u, &E[i].v, &E[i].cost);
+    }
+    int ans = kruskal(n, m);
+    printf("%d", ans);
+    return 0;
+}
+
---
+++ AlgoStruct10_5_Prim.cpp
@@ -0,0 +1,114 @@
+﻿#include <cstdio>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+const int MAXN = 1000;
+const int INF = 1000000000;
+
+struct Node
+{
+    int v, dis;
+    Node(int _v, int _dis) : v(_v), dis(_dis) {}
+};
+
+int G[MAXN][MAXN];
+vector<Node> Adj[MAXN];
+int n, m;
+int d[MAXN];
+bool vis[MAXN] = { false };
+
+int PrimAdj()
+{
+    int ans = 0;
+    fill(d, d + MAXN, INF);
+    d[0] = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        int u = -1, MIN = INF;
+        for (int j = 0; j < n; j++)
+        {
+            if (vis[j] == false && d[j] < MIN)
+            {
+                u = j;
+                MIN = d[j];
+            }
+        }
+
+        if (u == -1)
+        {
+            return -1;
+        }
+
+        vis[u] = true;
+        ans += d[u];
+
+        for (int j = 0; j < Adj[u].size(); j++)
+        {
+            int v = Adj[u][j].v;
+            int dis = Adj[u][j].dis;
+            if (vis[v] == false &&  dis < d[v])
+            {
+                d[v] = dis;
+            }
+        }
+
+    }
+    return ans;
+}
+
+int PrimMatrix()
+{
+    int ans = 0;
+    fill(d, d + MAXN, INF);
+    d[0] = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        int u = -1, MIN = INF;
+        for (int j = 0; j < n; j++) //找最小的d[u]
+        {
+            if (vis[j]==false && d[j] < MIN)
+            {
+                u = j;
+                MIN = d[j];
+            }
+        }
+        if (u == -1)
+        {
+            return -1;
+        }
+
+        vis[u] = true;
+        ans += d[u];
+        for (int v = 0; v < n; v++)
+        {
+            if (vis[v] == false && G[u][v] != INF && G[u][v] < d[v])
+            {
+                d[v] = G[u][v];
+            }
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    int u, v, w;
+    scanf_s("%d%d", &n, &m);
+    fill(G[0], G[0] + MAXN * MAXN, INF);
+    for (int i = 0; i < m; i++)
+    {
+        scanf_s("%d%d%d", &u, &v, &w);
+        G[u][v] = G[v][u] = w;
+        Adj[u].push_back(Node(v,w));
+        Adj[v].push_back(Node(u,w));
+    }
+    //int ansPrimMatrix = PrimMatrix();
+    int ansPrimAdj = PrimAdj();
+    printf("%d\n", ansPrimAdj);
+    return 0;
+}
+
