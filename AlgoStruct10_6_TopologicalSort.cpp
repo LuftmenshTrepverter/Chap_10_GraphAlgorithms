7a38dadbe3498a346f2d97edf576b652e6b245f4
LuftmenshTrepverter <71112445+LuftmenshTrepverter@users.noreply.github.com>
2021-06-14 14:03:13 +0800

    Add files via upload

---
+++ AlgoStruct10_6_TopologicalSort.cpp
@@ -0,0 +1,87 @@
+﻿#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <string>
+#include <map>
+using namespace std;
+const int MAXV = 100;
+
+vector<int> G[MAXV];
+int n, m, inDegree[MAXV], numperson = 0; //入度
+map<string, int> string2int; 
+map<int, string> int2string;
+
+int ChangeString(string str)
+{
+    if (string2int.find(str) != string2int.end())
+    {
+        return string2int[str];
+    }
+    else
+    {
+        string2int[str] = numperson;
+        int2string[numperson] = str;
+        return numperson++;
+    }
+}
+
+bool topologicalSort()
+{
+    int num = 0; //加入拓扑序列的顶点数
+    queue<int> q;
+    for (int i = 0; i < n; i++)
+    {
+        if (inDegree[i] == 0)
+        {
+            q.push(i);
+        }
+    }
+    while (!q.empty())
+    {
+        int u = q.front();
+        cout << int2string[u] << ' ';
+        q.pop();
+        for (int i = 0; i < G[u].size(); i++)
+        {
+            int v = G[u][i];
+            inDegree[v]--;
+            if (inDegree[v] == 0)
+            {
+                q.push(v);
+            }
+        }
+        G[u].clear();
+        num++;
+    }
+    cout << endl;
+    if (num == n)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int main()
+{
+    string su, sv, st;
+    scanf_s("%d%d", &n, &m);
+    for (int i = 0; i < m; i++)
+    {
+        cin >> su >> sv;
+        int idu = ChangeString(su);
+        int idv = ChangeString(sv);
+        G[idu].push_back(idv);
+        inDegree[idv]++;
+    }
+
+    bool canDAG = topologicalSort();
+
+    cout << canDAG << endl;
+    return 0;
+}
+
+
---
+++ AlgoStruct10_7_KeyPath.cpp
@@ -0,0 +1,69 @@
+﻿#include <cstdio>
+#include <iostream>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <string>
+using namespace std;
+
+const int MAXV = 100;
+
+struct Node
+{
+	int v, w;
+};
+
+int n, m, inDegree[MAXV], ve[MAXV];
+stack<int> topOrder; //拓扑排序
+vector<Node> G[MAXV];
+
+
+bool topologicalSort()
+{
+    queue<int> q;
+	for (int i = 0; i < n; i++)
+	{
+		if (inDegree[i] == 0)
+		{
+			q.push(i);
+		}
+	}
+	while (!q.empty())
+	{
+		int u = q.front();
+		q.pop();
+		topOrder.push(u);
+
+		for (int i = 0; i < G[u].size(); i++)
+		{
+			int v = G[u][i].v;
+			inDegree[v]--;
+			if (inDegree[v] == 0)
+			{
+				q.push(v);
+			}
+
+			if (ve[u] + G[u][i].w > ve[v])
+			{
+				ve[v] = ve[u] + G[u][i].w;
+			}
+		}
+	}
+	if (topOrder.size() == n)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+int main()
+{
+
+
+    return 0;
+}
+
+
